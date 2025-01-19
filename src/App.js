5ca09e3dd6e63d9a4d5d1942e789d51251c59417
huutamisme <hohuutam287@gmail.com>
2025-01-19 15:24:25 +0700

    Initialize react project with tailwindcss

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-red-200 flex items-center justify-center">
+      <div className="card w-96 bg-primary text-primary-content">
+        <div className="card-body">
+          <h2 className="card-title">SHUB</h2>
+          <p>Simple UI with daisyui</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-secondary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
